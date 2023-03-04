f525818e8c378141138e35c399c740b4b33620c1
Renisaaa <renisabhardwaj@gmail.com>
2023-03-04 13:20:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { ArrowLeft, ArrowRight} from "react-bootstrap-icons"
+
 
 function App() {
+  
+  const [ques, setQues] = useState();
+  const [index, setIndex] = useState(0);
+  const question = ["AreaUnderTheCurve_901", "BinomialTheorem_901", "DifferentialCalculus2_901"];
+  const getData = async () => {
+    let data = await fetch(`https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${question[index]}`);
+    data = await data.json();
+    setQues(data[0].Question);
+  }
+  useEffect(() => {
+    getData();
+  }, [index])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MathJaxContext>
+    <div className="container" style={{margin:"50px"}}>
+      
+
+      <div className="shadow-lg" style={{height:"200px", border:"1px solid black", borderRadius:"5px", padding:"10px"}}><strong>Ques{index+1}.</strong><MathJax>{ques}</MathJax></div>
+      <div style={{display:"flex", justifyContent:"center"}}>
+        <button style={{margin:"20px", height: "45px", width: "60px"}} className="btn btn-primary" disabled={index === 0} onClick={() => {setIndex(index - 1)}}><ArrowLeft/></button>
+        <button style={{margin:"20px",  height: "45px", width: "60px"}} className="btn btn-primary" disabled={index === 2} onClick={() => {setIndex(index + 1)}}><ArrowRight/></button>
+      </div>
     </div>
+    </MathJaxContext>
   );
 }
 
